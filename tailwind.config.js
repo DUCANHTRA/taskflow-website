6a0db8923101e156b41321757954b639d0f81cf8
trand <127272317+DUCANHTRA@users.noreply.github.com>
2025-08-26 08:09:34 +0700

    Add Tailwind CSS configuration and update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,11 @@
-content: [
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
-  ]
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
